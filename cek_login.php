308cea25f33adc446df513b778ce80a90df233fc
putrafahrezi <putrafahrezi89@gmail.com>
2024-01-04 10:15:59 +0700

    cek login

--- cek_login.php
+++ cek_login.php
@@ -7,4 +7,17 @@ if(!isset($_SESSION['sid']) AND !isset($_COOKIE['cid'])){
     </script>
     <?php
 }
+
+if(isset($_SESSION['sid'])){
+    //variabel element session
+    $l_id       = $_SESSION['sid'];
+    $l_nama     = $_SESSION['snama'];
+    $l_email    = $_SESSION['semail'];
+    
+}elseif(isset($_COOKIE['cid'])){
+    //variabel element cookie
+    $l_id = $_COOKIE['cid'];
+    $l_nama = $_COOKIE['cnama'];
+    $l_email = $_COOKIE['cemail'];
+}
 ?>
--- sidebar.php
+++ sidebar.php
@@ -13,7 +13,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?php echo $_SESSION['snama'] ?></a>
+          <a href="#" class="d-block"><?php echo $l_nama ?></a>
         </div>
       </div>
 
